96dfbf8a99ab015a20f5e9004e9471d00b0122aa
CollectorFTB <gilad.david95@gmail.com>
2017-11-05 17:44:41 +0200

    Fixed generateSymbolTable

--- compilers/homework1.cpp
+++ compilers/homework1.cpp
@@ -1,10 +1,18 @@
+// Homework-Collector.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <functional>
-#include <unordered_map>
+#include <map>
+#include <vector>
 #include <algorithm>
 using namespace std;
+
 
 // Abstract Syntax Tree
 class AST
@@ -16,11 +24,27 @@ public:
 
 
 	AST(string value, AST* left, AST* right) {
-		value = value;
-		left = left;
-		right = right;
+		this->value = value;
+		this->left = left;
+		this->right = right;
 	}
-	static AST* createAST(ifstream& input) {
+	/*
+	static void printTree(AST* head)
+	{
+		if (head == nullptr)
+		{
+			cout << "~" << endl;
+			return;
+		}
+			
+		cout << head->value << endl;
+		printTree(head->left);
+		printTree(head->right);
+	}
+	*/
+
+	static AST* createAST(fstream& input) {
+		
 		if (!(input))
 			return nullptr;
 
@@ -28,55 +52,85 @@ public:
 		getline(input, line);
 		if (line == "~")
 			return nullptr;
-
-		return new AST(line, createAST(input), createAST(input));
+		AST* left = createAST(input);
+		AST* right = createAST(input);
+		return new AST(line, left, right);
+		
 	}
 };
 typedef string types;
-typedef unsigned int address;
+typedef int address;
 class Variable {
 	// Think! what does a Variable contain?
 	//"CORRECTER" enum types {Int,Real,Bool};
 public:
 	string type;
 	address var_address;
-	Variable(address open_address,string type):var_address(open_address),type(type)
-	{ }
+	Variable() {}
+	Variable(address open_address, string type)
+	{
+		this->var_address = open_address;
+		this->type = type;
+	}
 };
 
 class SymbolTable {
 	// Think! what does a SymbolTable contain?
 	//std::unordered_map<
 public:
-		address free_address;
-		unordered_map<string,Variable> variable_table;
-		SymbolTable()
-		{
-			variable_table = unordered_map<string,Variable>();
-			free_address = 5;
-		}
-		Variable& operator[](string name){return this->variable_table[name];}
+	address free_address;
+	map<string, Variable> variable_table;
+	SymbolTable()
+	{
+		variable_table = map<string, Variable>();
+		free_address = 5;
+	}
+	Variable& operator[](string name) { return this->variable_table[name]; }
 	static SymbolTable generateSymbolTable(AST* tree) {
 		// TODO: create SymbolTable from AST
-			if(tree->value == "program")
-			return generateSymbolTable(tree->right->left);
-			if(tree->value == "scope")
-			{
-				SymbolTable return_table = SymbolTable();
-				for(AST* head=tree->left;head->left != nullptr; head = head->left)
-				{
-					AST* var = head->right;
-					string identifier = var->left->left->value;//var->identifier->a
-					string type = var->right->value;//var->real
-					return_table[identifier] = Variable(return_table.free_address++,type);
-				}
-				return return_table;
-			}
+		if (tree->value == "program")
+			return generateSymbolTable(tree->right);
+		if (tree->value == "content")
+		{
+			SymbolTable return_table = SymbolTable();
+			AST* head = tree->left->left;
+			fillSymbolTable(return_table, head);
+			return return_table;
+		}
+	}
+
+	static void fillSymbolTable(SymbolTable& table, AST* head)
+	{
+		if (head->left != nullptr)
+		{
+			fillSymbolTable(table, head->left);
+		}
+		AST* var = head->right;
+		string type = var->right->value;
+		string identifier = var->left->left->value;
+		table[identifier] = Variable(table.free_address++, type);
+
 	}
 	void print_table()
 	{
-	  for (std::unordered_map<string,Variable>::iterator it=variable_table.begin(); it!=variable_table.end(); ++it)
-	    std::cout << it->first << " => " << it->second.var_address << '\n';
+		std::vector<string> firstv;
+		std::vector<Variable> secondv;
+		for (auto const& imap : variable_table)
+		{
+			firstv.push_back(imap.first);
+			secondv.push_back(imap.second);
+		}
+		/*
+		for (std::map<string, Variable>::iterator it = variable_table.begin(); it != variable_table.end(); ++it)
+			std::cout << it->first << " => " << it->second.var_address << '	\n';
+		*/
+		for (int i = 0; i < firstv.size(); i++)
+		{
+			cout << firstv[i] << ',' << secondv[i].var_address << endl;
+		}
+		
+		
+
 	}
 };
 void generatePCode(AST* ast, SymbolTable symbolTable) {
@@ -84,18 +138,24 @@ void generatePCode(AST* ast, SymbolTable symbolTable) {
 }
 
 
-int main(int argc,char** argv)
+int main(int argc, char** argv)
 {
+
 	AST* ast;
-	ifstream myfile(argv[1]);
+	int x = 0;
+	fstream myfile("C:\\Users\\gilad\\Desktop\\Homework-Collector-master\\Homework-Collector-master\\compilers\\SamplesTxt\\tree5.txt");
 	if (myfile.is_open())
 	{
 		ast = AST::createAST(myfile);
 		myfile.close();
 		SymbolTable symbolTable = SymbolTable::generateSymbolTable(ast);
+		//AST::printTree(ast);
 		symbolTable.print_table();
-		generatePCode(ast, symbolTable);
+		//generatePCode(ast, symbolTable);
+		
 	}
 	else cout << "Unable to open file";
+
+	cin >> x;
 	return 0;
 }
