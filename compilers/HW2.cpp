bb2b9b325942b1a4653e54cb9c52d405a6926a9f
Tadaboody <tomerpet@gmail.com>
2017-12-19 15:57:15 +0200

    Set structure for homework 3

--- compilers/HW2.cpp
+++ compilers/HW2.cpp
@@ -7,9 +7,25 @@
 #include <utility>
 #include <memory>
 #include <stack>
+#include <locale>
 using namespace std;
 class SymbolTable;
+class Function;
+class AST;
+class Variable;
+class Array;
+class Pointer;
 // Abstract Syntax Tree
+#define data head->value
+enum ControlScope{While,Switch,None};
+void generatePCode(AST * ast, SymbolTable & symbolTable);
+void code(AST * head, SymbolTable & table,int loop_num,ControlScope scope);
+void execute_code(AST * head, SymbolTable & table,int loop_num,ControlScope scope);
+void load_expression(AST * head, SymbolTable & table);
+string load_variable(AST * head, SymbolTable & table);
+int generate_cases(AST* head, SymbolTable& table,int switch_num);
+void access_shift(AST* indexList, SymbolTable& table,const Array& array);
+
 class AST
 {
 public:
@@ -37,6 +53,7 @@ public:
 	}
 };
 
+
 typedef string types;
 typedef int address;
 class Variable {
@@ -45,6 +62,7 @@ public:
 	address var_address;
 	size_t size;
 	int offset;
+	string passing_type;
 	Variable() {}
 	Variable(address open_address, string type,int size,int offset):
 	var_address(open_address),type(type),offset(offset)
@@ -62,12 +80,12 @@ class Array : public Variable
 		int subpart;
 		int member_type_size;
 		string member_type;
-		unique_ptr<vector<pair<int,int>>> dimensions;
-		Array(address open_address,string type,int offset, string member_type ,int member_type_size, unique_ptr<vector<pair<int,int>>>& dimensions)
+		vector<pair<int,int>>* dimensions;
+		Array(address open_address,string type,int offset, string member_type ,int member_type_size, vector<pair<int,int>>* dimensions)
 		:Variable(open_address,type,calculate_size(*dimensions,member_type_size),offset)
 		{
 			this->member_type = member_type;
-			this->dimensions = move(dimensions);
+			this->dimensions = dimensions;
 			this->member_type_size = member_type_size;
 			calculate_subpart();
 		}
@@ -102,37 +120,16 @@ class Pointer : public Variable
 		,member_type(member_type)
 		{}
 };
-class Record : Variable
-{
-
-};
-
 
 class SymbolTable {
 public:
 	address free_address;
-	map<string, unique_ptr<Variable>> variable_table;
-	SymbolTable()
-	{
-		variable_table = map<string, unique_ptr<Variable>>();
-		free_address = 5;
-	}
+	map<string, Variable*> variable_table;
+	Function* pred_tree;
+	SymbolTable(Function* owner):free_address(5),pred_tree(owner)
+	{	}
 	Variable& operator[](string name) { return *this->variable_table[name]; }
-	static SymbolTable generateSymbolTable(AST* tree) {
-		// TODO: create SymbolTable from AST
-		SymbolTable return_table = SymbolTable();
-		if (tree->value == "program")
-			return generateSymbolTable(tree->right);
-		if (tree->value == "content")
-		{
-			if(tree->left != nullptr)
-			{
-				AST* head = tree->left->left;
-				fillSymbolTable(return_table, head);
-			}
-		}
-		return return_table;
-	}
+	static SymbolTable generateSymbolTable(AST* tree); 
 	map<string,int> size_table = 
 	{
 		{"int",1},
@@ -153,14 +150,14 @@ public:
 		string identifier = var->left->left->value;
 		address var_address = table.free_address;
 		int offset = var_address - scope_base;
-		unique_ptr<Variable> new_var;
+		Variable* new_var;
 		if(type=="array")
 		{
 			string member_type = var->right->right->value;
 			if(member_type == "identifier")
 				member_type = var->right->right->left->value;//botchy
 			int member_type_size = table.size_table[member_type];
-			unique_ptr<vector<pair<int,int>>> dimensions(new vector<pair<int,int>>());
+			vector<pair<int,int>>* dimensions= new vector<pair<int,int>>();
 			for(AST* rangeList = var->right->left;rangeList!=nullptr;rangeList=rangeList->left)
 			{
 				AST* range = rangeList->right;
@@ -168,47 +165,130 @@ public:
 				int end = stoi(range->right->left->value);
 				dimensions->push_back(make_pair(start,end));
 			}
-			new_var = unique_ptr<Array>(new Array(table.free_address,type,offset,member_type,member_type_size,dimensions));
+			new_var =  new Array(table.free_address,type,offset,member_type,member_type_size,dimensions);
 		}else if(type=="record")
 		{
 			int size = fillSymbolTable(table,var->right->left,var_address);
-			new_var = unique_ptr<Variable>(new Variable(var_address,type,size,offset));
+			new_var = new Variable(var_address,type,size,offset);
 		}
 		else if(type=="pointer")
 		{
 			string member_type = var->right->left->left->value;
-			new_var = unique_ptr<Variable>(new Pointer(table.free_address,type,table.size_table[type],offset,member_type));
+			new_var = new Pointer(table.free_address,type,table.size_table[type],offset,member_type);
 		}
 		else
 		{
-			new_var = unique_ptr<Variable>(new Variable(table.free_address,type,table.size_table[type],offset));
+			new_var = new Variable(table.free_address,type,table.size_table[type],offset);
 		}
-		table.variable_table[identifier] = move(new_var);
+		new_var->passing_type = var->value;//"var","byValue","byReference". quick fix so I don't change constructor. shows that I needed a factory all the while
+		table.variable_table[identifier] = (new_var);
 		table.free_address += type=="record"?0:table[identifier].size;
 		table.size_table[identifier] = table[identifier].size;
 		return table[identifier].size+scope_size;
 	}
-	void print_table()
-	{
-		for (auto const& imap : variable_table)
-		{
-			cout << imap.first << ',' << imap.second->var_address << endl;
-		}
-	}
+	// void merge(const SymbolTable& other_table) doesn't work because you can't copy unique pointers in copy constructors.
+	// {
+	// 	//https://stackoverflow.com/questions/3639741/merge-two-stl-maps
+	// 	// variable_table.insert(other_table.variable_table.begin(),other_table.variable_table.end());//map::merge is c++17 https://stackoverflow.com/questions/3639741/merge-two-stl-maps
+	// }
 };
+class Function {//the function you declare, not the one you pass as a var
+private:
+int find_extreme_pointer();
+public:
+	Function(AST* function_head,Function* static_link);
+	Function* static_link;
+	vector<Function*> children;
+	enum ArgumentType{ByValue,ByReference};
+	string name;
+	SymbolTable table;
+	SymbolTable arguments;
+	AST* statementsListHead;
+	void print_decleration();
+	void print_body();
+	// void createCall();
+};
+SymbolTable SymbolTable::generateSymbolTable(AST* tree) {
+		// TODO: create SymbolTable from AST
+		Function* main = new Function(tree,nullptr);
+		return main->table;
+	}
+int Function::find_extreme_pointer()
+{
+	return 1;
+}
+Function::Function(AST* function_head,Function* static_link):table(this),arguments(this)
+{
+	AST* id_and_parameters = function_head->left;
+	AST* parameters_list = id_and_parameters->right->left;
+	name = id_and_parameters->left->left->value;
+	this->static_link = static_link;
+	table = SymbolTable(this);
+	if(static_link != nullptr){
+		table.size_table = static_link->table.size_table;//"inherit" types
+		arguments.size_table = table.size_table;
+	}
+	AST* content = function_head->right;
+	statementsListHead = content->right;
+	AST* scope = content->left;
+	if(scope != nullptr){
+		SymbolTable::fillSymbolTable(table,scope->left);
+	if(parameters_list != nullptr)
+	{
+		SymbolTable::fillSymbolTable(arguments,parameters_list);
+		SymbolTable::fillSymbolTable(table,parameters_list);//is fine because I never use the size of this table, just the argument table
+		//arguments
+	}
+	// if(function_head->value == "function")
+	// {
+		//create a fake node for the return value,insert it into the table and 
+	// 	table.insert()
+	// }
+	AST* functionsList = scope->right;
+	for(AST* functionsList = scope->right;functionsList!=nullptr;functionsList=functionsList->left)
+	{
+		children.push_back(new Function(functionsList->right,this));
+	}
+}
+}
 
-enum ControlScope{While,Switch,None};
-void generatePCode(AST * ast, SymbolTable & symbolTable);
-void code(AST * head, SymbolTable & table,int loop_num,ControlScope scope);
-void execute_code(AST * head, SymbolTable & table,int loop_num,ControlScope scope);
-void load_expression(AST * head, SymbolTable & table);
-string load_variable(AST * head, SymbolTable & table);
-int generate_cases(AST* head, SymbolTable& table,int switch_num);
-void access_shift(AST* indexList, SymbolTable& table,const Array& array);
+string toupper(const string& str)
+{
+	string ret = string(str);
+	locale loc;
+	for (std::string::size_type i=0; i<str.length(); ++i)
+		ret[i] = toupper(ret[i],loc);//http://www.cplusplus.com/reference/locale/toupper/
+	return ret;
+}
 
+void Function::print_decleration()
+{
+	int sp = arguments.free_address;
+	int ep = find_extreme_pointer();
+	cout << toupper(name) << ":" << endl;
+	cout << "ssp " << sp << endl;
+	cout << "sep " << ep << endl;
+	cout << "ujp " << toupper(name) << "_begin" << endl;
+	for(Function* fun:children)
+	{
+		fun->print_decleration();
+	}
+}
+
+void Function::print_body()
+{
+	cout << toupper(name) << "_begin:" << endl;
+	
+	for(Function* fun:children)
+	{
+		fun->print_body();
+	}	
+}
 void generatePCode(AST* ast, SymbolTable& symbolTable) {
-	// TODO: go over AST and print code
-	code(ast->right->right,symbolTable,0,None);
+	// TODO: go over AST and print cod
+	Function* main = symbolTable.pred_tree;
+	main->print_decleration();
+	main->print_body();
 }
 void code(AST* head, SymbolTable& table,int loop_num,ControlScope scope) //gets StatementList
 {
@@ -222,7 +302,6 @@ void code(AST* head, SymbolTable& table,int loop_num,ControlScope scope) //gets 
 	execute_code(head->right, table,loop_num,scope);
 
 }
-#define data head->value
 int label_num = 0;
 
 
