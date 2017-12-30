d72c1911a9bd4554af07c85e270261b38e7d900a
CollectorFTB <gilad.david95@gmail.com>
2017-12-30 21:53:23 +0200

    fixed neighbors(?)

--- Machine Learning/Nearest_neighbors.py
+++ Machine Learning/Nearest_neighbors.py
@@ -1,22 +1,42 @@
 from scipy.io import loadmat
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import NearestNeighbors, KNeighborsClassifier
 from math import sqrt
 mats = loadmat('ABCD_data.mat')
-test_data = mats['test_data']
 train_data = mats['train_data']
+test_data = mats['test_data']
 valid_data = mats['valid_data']
+k_neighbors = list()
 success_rates = list()
 n = len(train_data)
-for k in range(20,int(sqrt(n))):
+max_k = 0
+max_rate = 0
+valid_data_length = len(valid_data)
+tags = list()
+
+t = ['a', 'b', 'c', 'd']
+
+for i in range(len(t)):
+    for j in range(500):
+        tags.append(t[i])
+
+for k in range(1, 150):
     success = 0
-    clf = NearestNeighbors(n_neighbors = k).fit(train_data)
-    distances,indices = clf.kneighbors(valid_data)
-    for index,neighbors in enumerate(indices):
-        k_i = 0
-        for neighbor in neighbors:
-            if neighbor//500 == index//100:
-                k_i+=1
-        if k_i >= k/4:
-            success+=1
-    success_rates.append((k,100*success/len(valid_data)))
-print(max(success_rates,key=lambda x:x[1]))
+    clf = KNeighborsClassifier(n_neighbors=k)
+    clf.fit(train_data, tags)
+
+    for index, element in enumerate(valid_data):
+        if t[index//100] == clf.predict([element]):
+            success += 1
+
+    success_rate = success/valid_data_length
+    k_neighbors.append(k)
+    success_rates.append(success_rate)
+
+    if success_rate == 0.9825:
+        print(k)
+
+    if max_rate < success_rate:
+        max_rate = success_rate
+        max_k = k
+
+print("success rate of", max_k, "neighbors:", max_rate)
