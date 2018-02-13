a780847e959d7c2041eb37463ab4e802dcf6b4ed
Tadaboody <tomerpet@gmail.com>
2018-02-14 00:43:43 +0200

    First messy draft of Fisher
    The jupyter version is more up-to-date but neither actually work
    probably some transposing problem

--- Machine Learning/Hw3/Face/FisherFaces.py
+++ Machine Learning/Hw3/Face/FisherFaces.py
@@ -1,14 +1,81 @@
-from np.linalg import eig
-from scipy.io import savemat
-def Normalize(T):
-    
-def FisherFaces(T):
 
-    savemat("FisherSpace.mat",F,mn)
-    
-%T is the Nxd training data for N=100 images (2 per person). d is the dimension of the features, representing an image.
-%Implement the code (see instruction in the pdf file)
-%F is the low-dimensional basis and mn if the mean of the training set.
- 
- save FisherSpace.mat F mn;
- 
+# coding: utf-8
+
+
+from scipy.io import loadmat
+import numpy as np
+import scipy
+mean_pic = 0
+
+
+def normalize(T):
+        global mean_pic
+        T = T.astype(float)
+        for index, sample in enumerate(T):
+            sample = np.true_divide(sample, sample.std())
+            sample = np.subtract(sample, sample.mean(0))
+            assert round(sample.mean(0), 2) == 0  # rounding errors may occur
+            assert round(sample.std(0), 2) == 1
+            sample -= mean_pic
+            T[index] = sample
+        if mean_pic is 0:
+            mean_pic = T.mean(0)
+            return normalize(T)
+        return T
+
+
+def FisherFaces(T):
+    N, d = T.shape  # number of samples,dimension of sample
+    c = N//2  # number of classes (people)
+    T = train_data
+    T = normalize(T)
+    A = T.T  # T-T
+    At = A.T  # = train_data
+
+    S_prime = At @ A
+
+    def k_best_eig_values_vectors(M, k, B=None):
+        a = scipy.linalg.eig(M, b=B)
+        eig_value_vector_pairs = [(value, vector)
+                                  for value, vector in zip(a[0], a[1])]
+        best_eig_value_vectors = sorted(
+            eig_value_vector_pairs, key=lambda x: x[0], reverse=True)[:k]
+        best_eig_values = np.array([pair[0]
+                                    for pair in best_eig_value_vectors])
+        best_eig_vectors = np.array([pair[1]
+                                     for pair in best_eig_value_vectors])
+        return best_eig_values, best_eig_vectors
+
+    best_eig_values, best_eig_vectors = k_best_eig_values_vectors(S_prime, N-c)
+
+    V = best_eig_vectors.T
+    Z = np.diag(best_eig_values)
+
+    W_pca = A @ V @ Z
+
+    proj_T = np.array([W_pca.T@t for t in T])
+    mean_pic = proj_T.mean(0)
+    face_pairs = np.array([(proj_T[i], proj_T[i+1])
+                           for i, t in enumerate(proj_T[:-1]) if i % 2 == 0])
+    mu = np.array([np.mean(pair, 0) for pair in face_pairs])
+
+    def S_i(i):
+        return sum(np.array([np.array([(face - mu[i])]).T @ np.array([(face - mu[i])]) for face in face_pairs[i]]))
+    S_w = sum(np.array([S_i(i) for i in range(c)]))
+    S_b = sum(np.array([2*np.array([(mu_i - mean_pic)]).T@
+                        np.array([(mu_i - mean_pic)]) for mu_i in mu]))
+
+    best_eig_values, best_eig_vectors = k_best_eig_values_vectors(
+        S_w, c-1, S_b)
+
+    W_mda = best_eig_vectors
+
+    W_opt = W_mda @ W_pca.T
+
+    return W_opt
+
+
+if __name__ == '__main__':
+    face_data = loadmat("FaceData.mat")
+    train_data = face_data["trainData"]
+    print(FisherFaces(train_data))
--- Machine Learning/Hw3/Face/recogTest.py
+++ Machine Learning/Hw3/Face/recogTest.py
@@ -1,8 +1,22 @@
-function suc=recogTest(trainData, testData)
-% trainData is a Nxd matrix of training data for N=100 images (2 per person). d is the dimension of the features, representing an image.
-% testData testData is a nxd matrix of test data for n=50 images (1 per person). d is the dimension of the features, representing an image.
-load FisherSpace.mat;
+# function suc=recogTest(trainData, testData)
+# % trainData is a Nxd matrix of training data for N=100 images (2 per person). d is the dimension of the features, representing an image.
+# % testData testData is a nxd matrix of test data for n=50 images (1 per person). d is the dimension of the features, representing an image.
+# load FisherSpace.mat;
 
-%suc is the number of correct classifications (when the index of the test image is equal to the predicted index)
-suc=suc/size(testData,1);
-fprintf('The recognition rate is %2.2f\n',suc);
+# %suc is the number of correct classifications (when the index of the test image is equal to the predicted index)
+# suc=suc/size(testData,1);
+# fprintf('The recognition rate is %2.2f\n',suc);
+N=100
+from scipy.io import loadmat
+from sklearn.neighbors import NearestNeighbors
+from FisherFaces import FisherFaces
+def recogTest(train_data,test_data):
+    fisher_space = loadmat("FisherSpace.mat")
+    train_data = fisher_space["trainData"]
+    test_data = fisher_space["testData"]
+    W_opt = FisherFaces(train_data)
+    face_pairs = np.array([(train_data[i],train_data[i+1]) for i,t in enumerate(train_data[:-1]) if i%2 == 0])
+    models = np.array([np.mean(pair,0) for pair in face_pairs])
+    proj_models = [W_opt.T @ model for model in proj_models]
+    suc = suc/len(filter(test_data)) 
+    return suc
